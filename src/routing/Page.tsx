5af2a4ca9f4db51789445be9eb8a01b2f1f8a1b7
BJ McCotter <bj.mccotter@adhocteam.us>
2022-06-22 11:07:54 -0500

    fix: another approach with recursion

--- src/routing/Page.tsx
+++ src/routing/Page.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Form, FormikContextType, useFormikContext } from 'formik';
 import { useNavigate, To, useSearchParams } from 'react-router-dom';
 import { IFormData, PageProps } from './types';
@@ -8,93 +8,51 @@ const validatePage = (
   children: JSX.Element[],
   state: FormikContextType<unknown>
 ): boolean => {
-  let isValid = true;
-
   const values = state.values as IFormData;
-  const requiredChildren = getRequiredChildren(children);
-  const requiredChildFields = getRequiredFormikFieldNames(requiredChildren);
+  const touchedValues = state.touched as IFormData;
+  const childFields = getChildFieldNames(children);
+  console.log(childFields);
 
-  requiredChildFields.map((fieldName) => {
-    isValid = isFieldEmptyOrErrored(fieldName, values, state);
+  console.log('-------------NEED SOME SPACE----------------');
+  console.log(state);
+
+  childFields.map((value) => {
+    if (value.split('.').length > 1) {
+      console.log('Complex Object');
+      console.log(values[`${value.split('.')[0]}`]);
+      touchedValues[`${value.split('.')[0]}`] =
+        values[`${value.split('.')[0]}`];
+    } else {
+      console.log('Simple Object');
+      console.log(values[`${value}`]);
+      touchedValues[`${value}`] = values[`${value}`];
+    }
   });
 
-  return isValid;
+  state.touched = touchedValues;
+
+  return false;
 };
 
-const isFieldEmptyOrErrored = (
-  fieldName: string,
-  values: IFormData,
-  state: FormikContextType<unknown>
-): boolean => {
-  let isValid = false;
-
-  const splitFieldNameList = fieldName.split('.');
-
-  const field = values[`${splitFieldNameList[0]}`] as IFormData;
-  console.log(field);
-  console.log(typeof field);
-
-  if (splitFieldNameList.length > 1) {
-    const field = values[`${splitFieldNameList[0]}`] as IFormData;
-    Object.entries(field).map(([key, value]) => {
-      if (key === splitFieldNameList[1]) {
-        if (
-          (typeof value === 'string' || typeof value === 'boolean') &&
-          value !== '' &&
-          state.errors !== {}
-        ) {
-          isValid = true;
-        } else {
-          isValid = false;
+const getChildFieldNames = (
+  children: JSX.Element[],
+  childFieldNames: string[] = []
+): string[] => {
+  if (children.length > 0) {
+    if (typeof children === 'object') {
+      children.map((childField) => {
+        if (childField.props?.name) {
+          childFieldNames.push(childField.props.name);
         }
-      }
-    });
-  } else {
-    const fieldValue = values[`${splitFieldNameList[0]}`];
-    if (
-      (typeof fieldValue === 'string' || typeof fieldValue === 'boolean') &&
-      fieldValue !== '' &&
-      state.errors !== {}
-    ) {
-      isValid = true;
-    } else {
-      isValid = false;
+
+        if (childField.props?.children) {
+          getChildFieldNames(childField.props?.children, childFieldNames);
+        }
+      });
     }
   }
 
-  return isValid;
-};
-
-const getRequiredFormikFieldNames = (children: JSX.Element[]): string[] => {
-  const requiredChildFields: string[] = [];
-
-  if (children.length > 0) {
-    children.map((childField) => {
-      requiredChildFields.push(childField.props.name);
-    });
-  }
-
-  return requiredChildFields;
-};
-
-const getRequiredChildren = (children: JSX.Element[]): JSX.Element[] => {
-  const requiredChildFields: JSX.Element[] = [];
-
-  console.log(children);
-
-  if (children.length > 0) {
-    children.forEach((child) => {
-      if (typeof child.props === 'object') {
-        const childPropsKeys = Object.keys(child.props);
-
-        if (childPropsKeys.indexOf('required') > -1) {
-          requiredChildFields.push(child);
-        }
-      }
-    });
-  }
-
-  return requiredChildFields;
+  return childFieldNames;
 };
 
 /**
@@ -110,6 +68,16 @@ export default function Page(props: PageProps): JSX.Element {
 
   const { nextRoute, previousRoute } = useContext(RouterContext);
   const state = useFormikContext();
+
+  useEffect(() => {
+    console.log(state);
+    // if(state.errors !== {}){
+    //   console.log(state.errors);
+    // }
+    // else{
+    //   console.log("STATE.ERRORS HAVE BEEN CLEARED")
+    // }
+  });
 
   return (
     <div>
@@ -137,8 +105,7 @@ export default function Page(props: PageProps): JSX.Element {
         {previousRoute && (
           <button
             className="btn usa-button-secondary prev"
-            onClick={(event) => {
-              event.preventDefault();
+            onClick={() => {
               navigate(previousRoute as To);
             }}
           >
@@ -147,9 +114,16 @@ export default function Page(props: PageProps): JSX.Element {
         )}
 
         {nextRoute && (
+          // IF THIS BUTTON DOES NOT ACT AS A SUBMISSION, THE FORMIK
+          // AUTOMATIC VALIDATION WILL NOT TRIGGER BECAUSE THE FIELDS
+          // ARE NOT BEING TOUCHED.
+          // type="button"  THIS WILL NOT SET ALL FIELDS TO TOUCHED
           <button
             className="btn usa-button-primary next"
+            type="submit" // IF YOU DON'T HAVE THIS, IT WILL DEFAULT TO 'submit' ANYWAY
             onClick={(event) => {
+              event.preventDefault(); // THIS ALSO PREVENTS FEILDS FROM BEING SET TO TOUCHED
+
               const isValid = validatePage(
                 props.children as JSX.Element[],
                 state
